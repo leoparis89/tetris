fc7f3d74b0220a16c1d660a85f7c22b7fa6fc7e6
levkowalski <levkowalski@gmail.com>
2018-02-05 23:10:57 +0100

    init things

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import Screen from '../../components/Screen/Screen';
 import './Game.scss';
-import pieces, {getRandomPiece} from './Pieces';
-import {addNewPiece} from './GameFuncs';
+import {getRandomPiece} from './Pieces';
 
 const getGrid = () => {
   const width = 10;
@@ -20,16 +19,40 @@ const getGrid = () => {
 };
 
 class Game extends Component {
-
     state = {
-      board: getGrid()
+      board: getGrid(),
+      currentBoard: null,
+      currentPos: {x: 0, y: 0},
+      currentPiece: null,
+      gameSpeed: 3000
+
     };
 
     componentDidMount() {
-      let board = addNewPiece(this.state.board);
-      this.setState({board});
+      this.setNewPiece(getRandomPiece());
+      setInterval(() => {
+        let {currentPos: {y}} = this.state;
+        y++;
+        this.setState({currentPos: {y}});
+      }, this.state.gameSpeed) ;
     }
 
+    setNewPiece(currentPiece) {
+      const {board} = this.state;
+      this.setState({
+        currentPiece,
+        currentPos: {
+          x: Math.floor((board[0].length - currentPiece.length) / 2),
+          y: 0
+        }
+      });
+    }
+    
+    updateCurrentBoard() {
+
+
+
+    }
 
     render() {
       const {board} = this.state;
