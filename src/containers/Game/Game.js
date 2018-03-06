8ede4b25dbb46b3ac511de4c81f25f938a522b6f
levkowalski <levkowalski@gmail.com>
2018-03-07 00:17:42 +0100

    indent

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -9,99 +9,100 @@ import Partie from '../../components/Partie/Partie';
 const newBoard = getGrid();
 
 class Game extends Component {
-    state = {
-      board: newBoard.slice(),
-      currentBoard: newBoard.slice(),
-      currentPos: {x: null, y: null},
-      currentPiece: null,
-      nextPiece: null,
-      gameSpeed: 1000,
-      intervalId: null, // if there's a number here the game is running
-      gameOver: false,
-      level: 1,
-      score: 0,
-      lines: 0,
-      effects: false,
-      welcome: true,
+  state = {
+    board: newBoard.slice(),
+    currentBoard: newBoard.slice(),
+    currentPos: {x: null, y: null},
+    currentPiece: null,
+    nextPiece: null,
+    gameSpeed: 1000,
+    intervalId: null, // if there's a number here the game is running
+    gameOver: false,
+    level: 1,
+    score: 0,
+    lines: 0,
+    effects: false,
+    welcome: true,
+  };
+
+
+  injectNextPiece = () => {
+    const {nextPiece} = this.state;
+    if (!nextPiece) return;
+    this.injectPiece(nextPiece);
+  };
+
+  /*
+   * Injects a piece on the top of the current Board
+   */
+  injectPiece = (piece) => {
+    const {board} = this.state;
+    const targetPos = {
+      x: Math.floor((board[0].length - piece.length) / 2),
+      y: 0
     };
+    const result = canPlace(board, piece, targetPos.x, targetPos.y);
 
-
-    injectNextPiece = () => {
-      const {nextPiece} = this.state;
-      if (!nextPiece) return;
-      this.injectPiece(nextPiece);
-    };
-
-    /*
-     * Injects a piece on the top of the current Board
-     */
-    injectPiece = (piece) => {
-      const {board} = this.state;
-      const targetPos = {
-        x: Math.floor((board[0].length - piece.length) / 2),
-        y: 0
-      };
-      const result = canPlace(board, piece, targetPos.x, targetPos.y);
-
-      if (result === BLOCKED) {
-        this.gameOver();
-      } else {
-        this.startFlow();
-        this.setState({
-          nextPiece: getRandomPiece(),
-          currentPiece: piece,
-          currentPos: targetPos,
-          currentBoard: place(board,piece, targetPos.x, targetPos.y)
-        });
-      }
-    }
-
-    gameOver = () => {
-      this.stopFlow();
-      this.setState({gameOver: true});
-    }
-
-    startNewGame = () => {
-      this.setState({gameOver: false, welcome: false});
-      this.reset();
+    if (result === BLOCKED) {
+      this.gameOver();
+    } else {
       this.startFlow();
+      this.setState({
+        nextPiece: getRandomPiece(),
+        currentPiece: piece,
+        currentPos: targetPos,
+        currentBoard: place(board, piece, targetPos.x, targetPos.y)
+      });
+    }
+  }
+
+  gameOver = () => {
+    this.stopFlow();
+    this.setState({gameOver: true});
+  }
+
+  startNewGame = () => {
+    this.setState({gameOver: false, welcome: false});
+    this.reset();
+    this.startFlow();
+  }
+
+  startFlow = () => {
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId);
     }
 
-    startFlow = () => {
-      if (this.state.intervalId) {
-        clearInterval(this.state.intervalId);
-      }
-
-      this.setState({intervalId: setInterval(()=> {
+    this.setState({
+      intervalId: setInterval(() => {
         this.move(DOWN);
       }, this.state.gameSpeed)
-      });
-    }
+    });
+  }
 
-    stopFlow = () => {
-      clearInterval(this.state.intervalId);
-      this.setState({
-        intervalId: null
-      });
-    }
+  stopFlow = () => {
+    clearInterval(this.state.intervalId);
+    this.setState({
+      intervalId: null
+    });
+  }
 
-    reset = () => {
-      this.setState({
-        board: newBoard.slice(),
-        currentBoard: newBoard.slice(),
-        currentPiece:null,
-        nextPiece: getRandomPiece(),
-        currentPos: null
-      });
-    }
+  reset = () => {
+    this.setState({
+      board: newBoard.slice(),
+      currentBoard: newBoard.slice(),
+      currentPiece: null,
+      nextPiece: getRandomPiece(),
+      currentPos: null
+    });
+  }
 
-    move = direction => {
-      const {board, currentPiece, currentPos, gameOver} = this.state;
-      // do nothing if no game is in progress
-      if (gameOver) return;
+  move = direction => {
+    const {board, currentPiece, currentPos, gameOver} = this.state;
+    // do nothing if no game is in progress
+    if (gameOver) return;
 
-      const targetPos = {...currentPos};
-      switch (direction) {
+    const targetPos = {...currentPos};
+    switch (direction) {
       case DOWN:
         targetPos.y++;
         break;
@@ -111,98 +112,99 @@ class Game extends Component {
       case RIGHT:
         targetPos.x++;
         break;
-      }
+    }
 
-      const result = canPlace(board, currentPiece, targetPos.x ,targetPos.y);
+    const result = canPlace(board, currentPiece, targetPos.x, targetPos.y);
 
-      if (result === CAN_PLACE) {
+    if (result === CAN_PLACE) {
+      this.setState({
+        currentBoard: place(board, currentPiece, targetPos.x, targetPos.y),
+        currentPos: targetPos
+      });
+    }
+
+    if (result === BLOCKED && direction === DOWN) {
+      const boardWithPiece = place(board, currentPiece, currentPos.x, currentPos.y);
+      this.handleNewBoard(boardWithPiece);
+    }
+  }
+
+  handleNewBoard(board) {
+    if (getFullLines(board)) {
+      this.stopFlow();
+      this.setState({effects: true});
+      setTimeout(() => {
         this.setState({
-          currentBoard: place(board, currentPiece, targetPos.x, targetPos.y),
-          currentPos: targetPos
+          board: removeCompletedLines(board),
+          effects: false,
+          lines: this.state.lines + getFullLines(board).length,
+          score: this.state.score + getFullLines(board).length * 10
         });
-      }
+        this.startFlow();
+      }, 2000);
 
-      if (result === BLOCKED && direction === DOWN) {
-        const boardWithPiece = place(board, currentPiece, currentPos.x, currentPos.y);
-        this.handleNewBoard(boardWithPiece);
-      }
+    } else {
+      this.setState({board: board});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.board !== prevState.board) {
+      this.injectNextPiece();
     }
 
-    handleNewBoard(board) {
-      if (getFullLines(board)) {
-        this.stopFlow();
-        this.setState({effects: true});
-        setTimeout(() => {
-          this.setState({board: removeCompletedLines(board),
-            effects: false,
-            lines: this.state.lines + getFullLines(board).length,
-            score: this.state.score + getFullLines(board).length*10
-          });
-          this.startFlow();
-        }, 2000 );
-
-      } else {
-        this.setState({board: board});
-      }
+    const {score, level} = this.state;
+    if (score >= level * 100) {
+      this.setState({level: level + 1});
     }
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-      if (this.state.board !== prevState.board) {
-        this.injectNextPiece();
-      }
+  handleMove = direction => {
+    if (!this.state.intervalId) return;
 
-      const {score, level} = this.state;
-      if (score >= level * 100) {
-        this.setState({level: level + 1});
-      }
+    this.move(direction);
+  }
+
+  handleRotate = (direction) => {
+    if (!this.state.intervalId) return;
+
+    const {board, currentPiece, currentPos: {x, y}} = this.state;
+    const rotatedPiece = rotate(currentPiece, direction === ROTATE_LEFT);
+    const result = canPlace(board, rotatedPiece, x, y);
+
+    if (result === CAN_PLACE) {
+      this.setState({
+        currentBoard: place(board, rotatedPiece, x, y),
+        currentPiece: rotatedPiece
+      });
     }
+  }
 
-    handleMove = direction => {
-      if (!this.state.intervalId) return;
-
-      this.move(direction);
-    }
-
-    handleRotate = (direction) => {
-      if (!this.state.intervalId) return;
-
-      const {board, currentPiece, currentPos: {x, y}} = this.state;
-      const rotatedPiece = rotate(currentPiece, direction === ROTATE_LEFT);
-      const result = canPlace(board, rotatedPiece, x, y);
-
-      if (result === CAN_PLACE) {
-        this.setState({
-          currentBoard: place(board, rotatedPiece, x, y),
-          currentPiece: rotatedPiece
-        });
-      }
-    }
-
-    render() {
-      const {currentBoard, gameOver, nextPiece, lines, score, level, effects, welcome} = this.state;
-      return (
-        <div className="container">
-          <div className="console">
-            <div className="screen-border">
-              <div className="screen">
-                {!gameOver &&
-                  <Partie board={currentBoard}
-                    effects={effects}
-                    nextPiece={nextPiece}
-                    lines={lines}
-                    score={score}
-                    level={level} />
-                }
-                {gameOver && <h1>Game Over</h1>}
-              </div>
+  render() {
+    const {currentBoard, gameOver, nextPiece, lines, score, level, effects, welcome} = this.state;
+    return (
+      <div className="container">
+        <div className="console">
+          <div className="screen-border">
+            <div className="screen">
+              {!gameOver &&
+              <Partie board={currentBoard}
+                      effects={effects}
+                      nextPiece={nextPiece}
+                      lines={lines}
+                      score={score}
+                      level={level}/>
+              }
+              {gameOver && <h1>Game Over</h1>}
             </div>
-            <Controller onMove ={this.handleMove}
-              onRotate={this.handleRotate}
-              onNewGame={this.startNewGame}
-            />
           </div>
-        </div>);
-    }
+          <Controller onMove={this.handleMove}
+                      onRotate={this.handleRotate}
+                      onNewGame={this.startNewGame}
+          />
+        </div>
+      </div>);
+  }
 }
 
 export default Game;
